dab108b8007682611cbda77ff48115df456a6760
Jonathan Lo <jonathandlo@live.com>
2020-08-13 07:05:33 -0700

    categories can be added and removed through ui

--- app/src/main/java/com/example/hangtimeassistant/ui/main/ViewCategories.kt
+++ app/src/main/java/com/example/hangtimeassistant/ui/main/ViewCategories.kt
@@ -1,26 +1,16 @@
 package com.example.hangtimeassistant.ui.main
 
-import android.graphics.Color
 import android.os.Bundle
-import android.os.Debug
-import android.provider.DocumentsContract
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.core.graphics.drawable.DrawableCompat
-import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import com.example.hangtimeassistant.*
-import com.google.android.material.textfield.TextInputEditText
 import kotlinx.android.synthetic.main.fragment_categories.*
-import kotlinx.android.synthetic.main.item_category.*
 import kotlinx.android.synthetic.main.item_category.view.*
-import kotlinx.android.synthetic.main.item_contact.view.*
-import kotlin.math.log
 
 /**
  * A placeholder fragment containing a simple view.
@@ -44,6 +34,12 @@ class ViewCategories : Fragment() {
         super.onActivityCreated(savedInstanceState)
 
         listCategories()
+
+        // configure the add categories button
+        button_cat_add.setOnClickListener {
+            val db = HangTimeDB.getDatabase(this.context!!)
+            addItem(db.categoryDao().getRow(db.categoryDao().insert(Category())), db)
+        }
     }
 
     private fun listCategories(){
@@ -51,33 +47,55 @@ class ViewCategories : Fragment() {
         val db = HangTimeDB.getDatabase(this.context!!)
         layout_cat_items.removeAllViews()
 
-            val newCatItem = layoutInflater.inflate(R.layout.item_category, null)
-            newCatItem.id = View.generateViewId()
-
-            // configure name textbox events
-            val nameEdit = newCatItem.textedit_cat_name
-            nameEdit.id = View.generateViewId()
-            nameEdit.setText(category.name)
-            nameEdit.addTextChangedListener(object:TextWatcher{
-                override fun afterTextChanged(s: Editable?) {
-                    if (nameEdit.hasFocus()) {
-                        db.categoryDao().update(category.apply { name = s.toString() })
-                    }
         for (category in db.categoryDao().getAll()){
-                }
-                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
-            })
-
-            // configure color button style
-            val colorButton = newCatItem.button_cat_color
-            colorButton.id = View.generateViewId()
-            colorButton.minimumWidth = colorButton.minimumHeight
-            colorButton.minWidth = colorButton.minHeight
-            DrawableCompat.setTint(DrawableCompat.wrap(colorButton.background), category.color)
-
-            layout_cat_items.addView(newCatItem)
+            addItem(category, db)
         }
+    }
+
+    private fun addItem(category: Category, db: HangTimeDB) {
+        val newCatItem = layoutInflater.inflate(R.layout.item_category, null)
+        newCatItem.id = View.generateViewId()
+
+        // configure name textbox events
+        val nameEdit = newCatItem.textedit_cat_name
+        nameEdit.id = View.generateViewId()
+        nameEdit.setText(category.name)
+        nameEdit.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                if (nameEdit.hasFocus()) {
+                    db.categoryDao().update(category.apply { name = s.toString() })
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
+
+        // configure color button style
+        val colorButton = newCatItem.button_cat_color
+        colorButton.id = View.generateViewId()
+        colorButton.minimumWidth = colorButton.minimumHeight
+        colorButton.minWidth = colorButton.minHeight
+        DrawableCompat.setTint(DrawableCompat.wrap(colorButton.background).mutate(), category.color)
+
+        // configure delete button
+        val delButton = newCatItem.button_cat_delete
+        delButton.id = View.generateViewId()
+        delButton.setOnClickListener {
+            it.isClickable = false
+            db.categoryDao().delete(category)
+            newCatItem.animate()
+                .alpha(0f)
+                .withEndAction {
+                    newCatItem.visibility = View.GONE
+                    layout_cat_items.removeView(newCatItem)
+                }
+        }
+
+        newCatItem.alpha = 0f
+        newCatItem.animate().alpha(1f)
+
+        layout_cat_items.addView(newCatItem)
     }
 
     companion object {
