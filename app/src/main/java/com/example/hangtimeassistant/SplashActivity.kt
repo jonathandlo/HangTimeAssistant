d32fdfc0e7e91ddc420ba4dbe0451dac3ef5aee1
Jonathan Lo <jonathandlo@live.com>
2021-01-17 21:13:35 -0800

    added a basic splash screen

---
+++ app/src/main/java/com/example/hangtimeassistant/SplashActivity.kt
@@ -0,0 +1,31 @@
+package com.example.hangtimeassistant
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.view.WindowManager
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or View.SYSTEM_UI_FLAG_FULLSCREEN
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Handler().postDelayed(
+            {
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            },
+            1500
+        )
+    }
+}
