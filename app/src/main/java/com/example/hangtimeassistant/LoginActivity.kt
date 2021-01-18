05f976012188b029baad1a3755d8d403639b20a5
Jonathan Lo <jonathandlo@live.com>
2021-01-17 23:19:24 -0800

    added a placeholder login screen

--- app/src/main/java/com/example/hangtimeassistant/LoginActivity.kt
+++ app/src/main/java/com/example/hangtimeassistant/LoginActivity.kt
@@ -3,29 +3,25 @@ package com.example.hangtimeassistant
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
 import android.view.View
 import android.view.WindowManager
+import kotlinx.android.synthetic.main.activity_login.*
 
-class SplashActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        setContentView(R.layout.activity_login)
 
-        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN)
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or View.SYSTEM_UI_FLAG_FULLSCREEN
+
+        button_login_login.setOnClickListener { onLoginClick() }
     }
 
-    override fun onStart() {
-        super.onStart()
-
-        Handler().postDelayed(
-            {
-                startActivity(Intent(this, LoginActivity::class.java))
-                finish()
-            },
-            1500
-        )
+    fun onLoginClick(){
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 }
