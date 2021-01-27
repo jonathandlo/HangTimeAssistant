153cd6dc9c5343b01dbf5d324f2e4fa152dd1fc8
Jonathan Lo <jonathandlo@live.com>
2021-01-27 04:34:32 -0800

    code cleanup

--- app/src/main/java/com/example/hangtimeassistant/SplashActivity.kt
+++ app/src/main/java/com/example/hangtimeassistant/SplashActivity.kt
@@ -12,7 +12,8 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN)
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or View.SYSTEM_UI_FLAG_FULLSCREEN
     }
