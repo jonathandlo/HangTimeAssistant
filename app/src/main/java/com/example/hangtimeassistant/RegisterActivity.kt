ef08ccd2d9f9fe74b4c6f33b3a41e7638f94f943
Jonathan Lo <jonathandlo@live.com>
2021-01-27 04:34:03 -0800

    added a barebones account registration screen

--- app/src/main/java/com/example/hangtimeassistant/RegisterActivity.kt
+++ app/src/main/java/com/example/hangtimeassistant/RegisterActivity.kt
@@ -1,30 +1,18 @@
 package com.example.hangtimeassistant
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
-import kotlinx.android.synthetic.main.activity_login.*
 
-class LoginActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_register)
 
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN)
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or View.SYSTEM_UI_FLAG_FULLSCREEN
-
-        // add event handlers
-        login_text_register.setOnClickListener {
-            startActivity(Intent(this, RegisterActivity::class.java))
-        }
-        button_login_login.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
-        }
     }
-
 }
