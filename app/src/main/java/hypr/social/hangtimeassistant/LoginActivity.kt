170a8fca580d52b4aa58806bcd4652db5ee51547
Jonathan Lo <jonathandlo@live.com>
2021-01-29 04:46:27 -0800

    added working login functionality

--- app/src/main/java/hypr/social/hangtimeassistant/LoginActivity.kt
+++ app/src/main/java/hypr/social/hangtimeassistant/LoginActivity.kt
@@ -3,8 +3,12 @@ package hypr.social.hangtimeassistant
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
@@ -14,20 +18,42 @@ class LoginActivity : AppCompatActivity() {
 
         // add event handlers
         text_login_register.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+        button_login_login.setOnClickListener {
             val email = text_login_email.text.toString().trim()
             val password = text_login_password.text.toString().trim()
 
-            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
-                .addOnSuccessListener {
+            if (!validateLogin(email, password)) return@setOnClickListener
 
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                .addOnSuccessListener(this) { task ->
+                    proceed()
                 }
                 .addOnFailureListener {
-
+                    Toast.makeText(baseContext, "Authentication failed: " + it.message, Toast.LENGTH_LONG).show()
                 }
         }
-        button_login_login.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        // skip login if user is already signed in
+        //if (FirebaseAuth.getInstance().currentUser != null) proceed()
+    }
+
+    private fun proceed(){
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+
+    private fun validateLogin(email: String, password: String) : Boolean {
+        when {
+            email.isEmpty() -> Toast.makeText(baseContext, "Please enter an email address", Toast.LENGTH_LONG).show()
+            password.isEmpty() -> Toast.makeText(baseContext, "Please enter a password", Toast.LENGTH_LONG).show()
+            else -> return true
         }
+        return false
     }
 }
